feeaed68f400038181e60f3b92b0736b4342b1dd
songta <sith.norvang@keyprod.com>
2022-10-20 07:36:07 +0200

    potatoe top gain for a day

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :potatoe_prices, only: [:index]
+  resources :potatoe_prices, only: [:index] 
+  get '/potatoe_prices/maximal_gain', to: 'potatoe_prices#maximal_gain', as: 'maximal_gain'
 end
